3cdd09cf60272dc3c177faf41ebf57ea2445da64
ShineShao <xiaoshaoqq@gmail.com>
2026-02-19 17:42:20 +0800

    ci: remove node 14.x 16.x

--- jest.config.js
+++ jest.config.js
@@ -81,9 +81,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
-    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
-    '^@/core/(.*)$': '<rootDir>/src/core/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
